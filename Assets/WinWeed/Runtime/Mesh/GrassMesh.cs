20c1a1e6936eb607fdbd01acba275d79e5be72a7
Hsinpa <hsinpa@gmail.com>
2022-08-01 16:55:20 +0800

    first commit

---
+++ Assets/WinWeed/Runtime/Mesh/GrassMesh.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Hsinpa.Winweed {
+    public class GrassMesh
+    {
+        public GrassMesh() { 
+        
+        }
+
+        public Mesh CreateMesh() {
+            Mesh m = new Mesh();
+
+            return m;
+        }
+
+        private void CreateVertice() { 
+        
+        }
+
+        private void CreateTriangle()
+        {
+
+        }
+
+        private void CreateUV()
+        {
+
+        }
+
+        private void CreateNormal() { 
+        
+        }
+
+
+    }
+}
